f647f35dd30baf82aef5a941c19cbb60b7960404
NemGame <119305977+NemGame@users.noreply.github.com>
2025-09-06 12:57:38 +0200

    Create input.cpp

---
+++ libs/cpp/input.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+string input(string pre = "", string post = "\n") {
+	cout << pre;
+	char ch;
+	string out = "";
+	while (1) {
+		ch = _getch();
+		if (ch == 13) break;
+		if (ch == 8) {
+			if (out.length() > 0) {
+				out.pop_back();
+				cout << "\b \b";
+			}
+			continue;
+		}
+		out.push_back(ch);
+		cout << ch;
+	}
+	cout << post;
+	return out;
+}
+
+long inputLong(string pre = "", string post = "\n") {
+	string out = input(pre, post);
+  return out.empty() ? 0 : strtol(out.c_str(), NULL, 10);
+}
+
+int inputInt(string pre = "", string post = "\n") {
+  return inputLong(pre, post);
+}
